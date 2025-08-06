7fb4ac4f3fe035fa60f80978349cd1dd97480054
MohammedHassan <hasssan.noorisys@gmail.com>
2025-08-06 16:16:42 +0530

    REF: used chatModel.create instead of .save()

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -4,9 +4,7 @@ import groupModel from '../models/group.model.js'
 // send message service
 const sendGroupMessageService = async (userId, message, groupId) => {
 
-    const newMessage = new chatModel({ senderId: userId, message: message })
-
-    await newMessage.save()
+    const newMessage = await chatModel.create({ senderId: userId, message: message })
 
     if (groupId)
         await groupModel.updateOne({ _id: groupId }, { $push: { chats: newMessage._id } })
